acd33a502189556ab5ceda0e57ac5548c503f50b
Ignacio Gonzalez <igonzalez@rid.cl>
2026-05-05 18:06:02 -0400

    asd

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -1,6 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
-require("dotenv/config");
 const client_1 = require("@prisma/client");
 exports.prisma = new client_1.PrismaClient();
+exports.prisma
+    .$connect()
+    .then(() => {
+    console.log("Prisma conectado correctamente");
+})
+    .catch((error) => {
+    console.error("Error conectando Prisma:", error);
+    process.exit(1);
+});
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,13 @@
-import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
+
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Prisma conectado correctamente");
+  })
+  .catch((error) => {
+    console.error("Error conectando Prisma:", error);
+    process.exit(1);
+  });
