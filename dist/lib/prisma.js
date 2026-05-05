1995037ddf29e553f410a50332732ec2026cb5f0
Ignacio Gonzalez <igonzalez@rid.cl>
2026-05-05 17:16:47 -0400

    arreglo prisma

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -3,10 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 require("dotenv/config");
 const client_1 = require("@prisma/client");
-const adapter_pg_1 = require("@prisma/adapter-pg");
-const adapter = new adapter_pg_1.PrismaPg({
-    connectionString: process.env.DATABASE_URL,
-});
-exports.prisma = new client_1.PrismaClient({
-    adapter,
-});
+exports.prisma = new client_1.PrismaClient();
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,4 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
-});
-
-export const prisma = new PrismaClient({
-  adapter,
-});
+export const prisma = new PrismaClient();
