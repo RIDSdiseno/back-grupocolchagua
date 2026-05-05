2104d855a7e544cebb829c58c41ce7bbbeacffb9
Ignacio Gonzalez <igonzalez@rid.cl>
2026-05-05 17:28:15 -0400

    cors

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(Number(PORT), "0.0.0.0", () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
